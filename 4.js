cd30e33d142177bce1feae2542de89e9916aa98a
MelnikDmitriy <modie8856@gmail.com>
2023-05-24 10:34:45 +0300

    feat: add jsFour

---
+++ 4.js
@@ -0,0 +1,13 @@
+function filter(num) {
+    a = b.split(" ");
+    return {
+        fullname: a[0],
+        lastname: a[1],
+        patronimicname: a[2]
+    }
+}
+
+let number = '88005553535';
+filter(number);
+
+
